970883ec008cb4a1bcf345d42e369e36a4f9503f
Dimitar <deadmitko@gmail.com>
2019-02-23 12:32:58 +0200

    Proba commit 2

--- Proba/Proba/Program.cs
+++ Proba/Proba/Program.cs
@@ -11,6 +11,7 @@ namespace Proba
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
+            Console.WriteLine("Hello world 2");
         }
     }
 }
